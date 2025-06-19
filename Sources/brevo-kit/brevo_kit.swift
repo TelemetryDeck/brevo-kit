7af1d36cf6e6f42d081d2d22a6cc293d5c26102d
Daniel Jilg <daniel@telemetrydeck.com>
2025-06-19 11:42:21 +0200

    Initial Commit

---
+++ Sources/brevo-kit/brevo_kit.swift
@@ -0,0 +1,5 @@
+import OpenAPIRuntime
+import OpenAPIURLSession
+
+// let client = Client(serverURL: try Servers.Server1.url(), transport: URLSessionTransport())
+// let response = try await client.getProcess(.init(path: .init(processId: 39)))
